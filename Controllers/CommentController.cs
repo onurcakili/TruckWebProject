a5b65f02c267a55f695c5ba1f187021407f69359
Onur Çakılı <onurcakili0@gmail.com>
2023-04-15 01:35:14 +0300

    Initial commit

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics.X86;
+
+namespace TruckWebProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager cm = new CommentManager(new EFCommentRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetCommentListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TruckWebProject.Controllers
+{
+	public class RegisterController : Controller
+	{
+		DriverManager dm = new DriverManager(new EFDriverRepository());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Driver p)
+		{
+			DriverValidatior dv = new DriverValidatior();	
+			ValidationResult results = dv.Validate(p);
+			if(results.IsValid)
+			{
+                p.DriverStatus = true;
+                dm.DriverAdd(p);
+
+                return RedirectToAction("Index", "Comment");
+            }
+			else
+			{
+				foreach(var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+				return View();
+			}
+			
+		}
+	}
+}
